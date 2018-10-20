a4f709357ec8b3f55b89dede22accfd350f16df4
CarlosDnl <c.ospina2001@pascualbravo.edu.co>
2018-10-20 17:56:47 -0500

    Primer ejercicio

---
+++ 1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacturaDesinfectante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese su nombre: "); //Solicitando el nombre
+            string nombre = Console.ReadLine(); //Almacenando el nombre en la variable "nombre"
+            Console.WriteLine(nombre + " estas usando mi programa"); //Uniendo el texto en la varible nombre con el mensaje
+            Console.ReadKey();
+
+        }
+    }
+}
